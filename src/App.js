b335f27f402039ce7917b477e042284ec98e438e
ruizhen88 <yejin@workmate.asia>
2021-07-22 11:49:58 +0900

    rendering data fetched from api and added antd components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import { Switch, Route } from "react-router-dom";
 import MovieListPage from "./components/page/MovieListPage/index";
 import MovieDetailPage from "./components/page/MovieDetailPage/index";
 import NotFound from "./components/UI/NotFound";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+const { Header, Content } = Layout;
 
 function App() {
-	return (
-		<div>
-			<Switch>
-				<Route exact path="/" component={MovieListPage}></Route>
-				<Route path="/detail/:id" component={MovieDetailPage}></Route>
-				<Route component={NotFound}></Route>
-			</Switch>
-		</div>
-	);
+  return (
+    <Layout className="layout">
+      <Header />
+      <Content>
+        <Switch>
+          <Route exact path="/" component={MovieListPage}></Route>
+          <Route path="/detail/:id" component={MovieDetailPage}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
--- src/components/page/MovieListPage/index.js
+++ src/components/page/MovieListPage/index.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import movieData from "../../../movies.json";
+import axios from "axios";
+import { Card, Row, Col, Spin } from "antd";
 
 const MovieListPage = () => {
-	return (
-		<>
-			{movieData.map((movie) => (
-				<Link key={movie.id} to={`/detail/${movie.id}`}>
-					<div>
-						<div>
-							Thumbnail <span>{movie.rating}</span>
-						</div>
-						<h3>{`${movie.title} (${movie.year})`}</h3>
-						<p>{movie.director}</p>
-						{movie.categories.map((category) => (
-							<span key={Math.random()}>{category} </span>
-						))}
-						<hr />
-					</div>
-				</Link>
-			))}
-		</>
-	);
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://my-json-server.typicode.com/ruizhen88/demo/movies"
+      );
+      setMovies(res.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {loading && <Spin />}
+
+      <Row>
+        {movies.map((movie) => (
+          <Col span={8}>
+            <Card>
+              <Link key={movie.id} to={`/detail/${movie.id}`}>
+                <div>
+                  <div>
+                    Thumbnail <span>{movie.rating}</span>
+                  </div>
+                  <h3>{`${movie.title} (${movie.year})`}</h3>
+                  <p>{movie.director}</p>
+                  {movie.categories.map((category) => (
+                    <span key={Math.random()}>{category} </span>
+                  ))}
+                  <hr />
+                </div>
+              </Link>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default MovieListPage;
