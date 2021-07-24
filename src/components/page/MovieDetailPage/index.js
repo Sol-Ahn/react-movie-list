362157cb6547e64a144dcec976918df029ed3054
Sol-Ahn <sorry0194@gmail.com>
2021-07-25 00:25:48 +0900

    Add: axios GET API call

--- src/components/page/MovieDetailPage/index.js
+++ src/components/page/MovieDetailPage/index.js
@@ -1,21 +1,35 @@
-import React from "react";
-import movieData from "../../../movies.json";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const MovieDetailPage = (props) => {
-	const movie = movieData.find((movie) => movie.id === +props.match.params.id);
+	const [movie, setMovie] = useState({});
+	const [loading, setLoading] = useState(false);
+	const movieId = props.match.params.id;
 
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				setLoading(true);
+				const res = await axios.get(
+					`https://my-json-server.typicode.com/ruizhen88/demo/movies/${movieId}`
+				);
+				setMovie(res.data);
+				setLoading(false);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchData();
+	}, [movieId]);
 	return (
 		<>
+			{loading}
 			<div>
 				<div>Thumbnail</div>
 				<strong>감독</strong>
 				<p>{movie.director}</p>
 				<strong>장르</strong>
-				<p>
-					{movie.categories.map((category) => (
-						<span key={Math.random()}>{category} </span>
-					))}
-				</p>
+				<p>{movie.categories}</p>
 			</div>
 			<div>
 				<h2>
--- src/components/page/MovieListPage/index.js
+++ src/components/page/MovieListPage/index.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import movieData from "../../../movies.json";
+import axios from "axios";
 
 const MovieListPage = () => {
+	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		// axios
+		// 	.get("https://my-json-server.typicode.com/ruizhen88/demo/movies")
+		// 	.then((res) => {
+		// 		console.log(res);
+		// 		setMovies(res.data);
+		// 	})
+		// 	.catch((err) => {
+		// 		console.log(err);
+		// 	});
+
+		const fetchData = async () => {
+			try {
+				setLoading(true);
+				const res = await axios.get(
+					"https://my-json-server.typicode.com/ruizhen88/demo/movies"
+				);
+				setMovies(res.data);
+				setLoading(false);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<>
-			{movieData.map((movie) => (
+			{loading}
+			{movies.map((movie) => (
 				<Link key={movie.id} to={`/detail/${movie.id}`}>
 					<div>
 						<div>
