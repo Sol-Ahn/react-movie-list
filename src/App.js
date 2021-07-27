a1fc28a26bd95823bdf0edfe5e23a478369a7adb
Sol-Ahn <sorry0194@gmail.com>
2021-07-27 18:44:23 +0900

    Add: detail page layout, MovieFormPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import MovieListPage from "./components/page/MovieListPage/index";
 import MovieDetailPage from "./components/page/MovieDetailPage/index";
+import MovieFormPage from "./components/page/MovieFormpage/index";
 import NotFound from "./components/UI/NotFound";
 import StyledHeader from "./components/UI/Header";
 import "./App.css";
@@ -14,12 +15,15 @@ const App = () => {
 	return (
 		<Layout>
 			<StyledHeader>
-				<div className="logo">My Movies</div>
+				<Link to="/">
+					<span className="logo">My Movies</span>
+				</Link>
 			</StyledHeader>
-			<Content style={{ paddingTop: "5%" }}>
+			<Content style={{ paddingTop: "5%", maxWidth: "80%", margin: "0 auto" }}>
 				<Switch>
 					<Route exact path="/" component={MovieListPage}></Route>
 					<Route path="/detail/:id" component={MovieDetailPage}></Route>
+					<Route path="/form" component={MovieFormPage}></Route>
 					<Route component={NotFound}></Route>
 				</Switch>
 			</Content>
